a10d2e160f3667cbc54b654b0fe47f084429b497
kkata <14849535+kkata@users.noreply.github.com>
2024-07-01 14:56:09 +0900

    Registering the TypeORM module in the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from '../config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        entities: ['dist/**/entities/**/*.entity.js'],
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
