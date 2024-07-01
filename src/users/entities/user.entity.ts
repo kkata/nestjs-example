bfda3f5a24265d803897836851846ba202b010fe
kkata <14849535+kkata@users.noreply.github.com>
2024-07-01 14:54:34 +0900

    Define the database tables

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn({
+    comment: 'accountID',
+  })
+  readonly id: number;
+
+  @Column('varchar', {
+    comment: 'accountName',
+  })
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
