2cd332ca32376cd36b0ee98e0e295cbdea9eca2f
kkata <14849535+kkata@users.noreply.github.com>
2024-07-02 10:36:08 +0900

    Implementing the API to create a user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async create({ name }: CreateUserDto): Promise<User> {
+    return await this.userRepository.save(new User(name)).catch((e) => {
+      throw new InternalServerErrorException(
+        `Failed to create user: ${e.message}`,
+      );
+    });
   }
 }
